49dc5add164569ab56ae3d0550ad35bcf8ebd5ee
Dmitry Kvasnikov <dmitry.kvasnikov@gmail.com>
2024-04-01 21:02:10 +0200

    2016 AOC preparation

---
+++ 2016/template/Main.hs
@@ -0,0 +1,20 @@
+module Main where
+
+type Input = String
+
+getInput :: FilePath -> IO Input
+getInput filepath = do
+  input <- readFile filepath
+  return input
+
+part1 :: Input -> Int
+part1 input = 42
+
+part2 :: Input -> Int
+part2 input = 42
+
+main :: IO()
+main = do
+  input <- getInput "./input.txt"
+  putStrLn $ "Part 1: " <> show (part1 input)
+  putStrLn $ "Part 2: " <> show (part2 input)
