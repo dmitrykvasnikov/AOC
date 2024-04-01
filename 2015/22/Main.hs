a5855ee4799709cb8dfd854d117d25ffe68bc429
Dmitry Kvasnikov <dmitry.kvasnikov@gmail.com>
2024-04-01 12:27:39 +0200

    minor fixes

--- 2015/22/Main.hs
+++ 2015/22/Main.hs
@@ -4,7 +4,7 @@ import Data.Map.Strict qualified as M
 import Data.Maybe (fromJust)
 import Data.List (sort)
 
-data Player = Player { pm, ph, pa :: Int}
+data Player = Player { pm, ph :: Int}
 data Boss = Boss { bh, bd :: Int}
 data Spell = Missile | Drain | Shield | Poison | Recharge deriving (Eq, Ord)
 
@@ -34,9 +34,9 @@ cast Shield game = game { spells = M.insertWith (+) Shield 6 $ spells game }
 cast Poison game = game { spells = M.insertWith (+) Poison 6 $ spells game }
 cast Recharge game = game { spells = M.insertWith (+) Recharge 5 $ spells game }
 
-evalGame :: Game -> Int -> [Int]
+evalGame :: Game -> Bool -> [Int]
 evalGame (Game p b s t c) d = case t of
-  P | bh b <= 0 -> [c]
+  P | bh boss' <= 0 -> [c]
     | ph player' <= 0 -> [-1]
     | otherwise ->
       let possibleSpells = filter (\sp -> (snd sp <= pm p) && (not $ M.member (fst sp) s)) costs
@@ -44,11 +44,13 @@ evalGame (Game p b s t c) d = case t of
          else concatMap go $ map fst possibleSpells where
            go sp = evalGame (cast sp $ Game { player = player'' {pm = pm player'' - cost'}, boss = boss', spells =  spells', turn = (next t), cost = (c + cost') }) d
             where cost' = fromJust . lookup sp $ costs
-  B -> evalGame (Game { player = player'' { ph = ph player'' - max 1 (bd b - armor) }, boss = boss', spells = spells', turn = next t, cost = c }) d
+  B | bh boss' <=0 -> [c]
+    | otherwise ->
+      evalGame (Game { player = player'' { ph = ph player'' - max 1 (bd b - armor) }, boss = boss', spells = spells', turn = next t, cost = c }) d
   where
     spells' = M.filter (>0) $ subtract 1 <$> s
     boss' = foldr castBoss b $ M.keys s
-    player' = if t == B then p else p { ph = ph p - d }
+    player' = if t == P && d  then p { ph = ph p - 1 } else p
     player'' = foldr castPlayer player' $ M.keys s
     armor = if M.member Shield s then 7 else 0
 
@@ -60,21 +62,14 @@ castPlayer :: Spell -> Player -> Player
 castPlayer Recharge p = p { pm = pm p + 101 }
 castPlayer _ p        = p
 
-type Input = String
-
-getInput :: FilePath -> IO Input
-getInput filepath = do
-  input <- readFile filepath
-  return input
-
 part1 :: Int
-part1 = head . sort . filter (/=(-1)) . evalGame  (Game (Player 500 50 0) (Boss 51 9) M.empty P 0) $ 0
+part1 = head . sort . filter (/=(-1)) . evalGame  (Game (Player 500 50) (Boss 51 9) M.empty P 0) $ False
 
 part2 :: Int
-part2 = head . sort . filter (/=(-1)) . evalGame  (Game (Player 500 50 0) (Boss 51 9) M.empty P 0) $ 1
+part2 = head . sort . filter (/=(-1)) . evalGame  (Game (Player 500 50) (Boss 51 9) M.empty P 0) $ True
+
 
 main :: IO()
 main = do
-  input <- getInput "./input.txt"
-  putStrLn $ "Part 1: " <> show (part1)
+--  putStrLn $ "Part 1: " <> show (part1)
   putStrLn $ "Part 2: " <> show (part2)
