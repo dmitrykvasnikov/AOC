f4854321ca20ff3cdaa94622a82c5d064f989896
Dmitry Kvasnikov <dmitry.kvasnikov@gmail.com>
2024-04-12 12:57:29 +0200

    minor fixes

---
+++ 2016/13/Main.hs
@@ -0,0 +1,42 @@
+module Main where
+
+import Data.Bits
+import Data.Maybe (fromMaybe, catMaybes)
+import Text.Regex.Applicative (RE, match, (=~), sym, psym, string, many, some, (<|>))
+import Text.Regex.Applicative.Common (decimal)
+
+-- /** Utilities
+type Parser a = RE Char a
+
+sepBy :: Parser a -> Parser b -> Parser [a]
+sepBy p s = (:) <$> p <*> many (s *> p)
+
+plus, minus :: forall a . (Enum a, Bounded a) => a -> Int -> a
+plus v n = toEnum ((fromEnum v + n) `mod`  (fromEnum (maxBound :: a) + 1))
+minus v n = plus v (-n)
+inc, dec :: forall a . (Enum a, Bounded a) => a -> a
+inc v = plus v 1
+dec v = plus v (-1)
+-- **/
+
+type Input = String
+
+getInput :: FilePath -> IO Input
+getInput filepath = do
+  input <- prepare <$> readFile filepath
+  return input
+
+prepare :: String -> Input
+prepare = undefined
+
+part1 :: Input -> Int
+part1 input = 42
+
+part2 :: Input -> Int
+part2 input = 42
+
+main :: IO()
+main = do
+  input <- getInput "./input.txt"
+  putStrLn $ "Part 1: " <> show (part1 input)
+  putStrLn $ "Part 2: " <> show (part2 input)
