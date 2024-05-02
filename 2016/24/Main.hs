938b36e7b009a35c0037e21512f101ab88c726bd
Dmitry Kvasnikov <dmitry.kvasnikov@gmail.com>
2024-05-02 15:19:16 +0200

    minor fixes

--- 2016/24/Main.hs
+++ 2016/24/Main.hs
@@ -3,7 +3,9 @@ module Main where
 import Data.Maybe (fromMaybe, catMaybes)
 import Text.Regex.Applicative (RE, match, (=~), sym, psym, string, many, some, (<|>))
 import Text.Regex.Applicative.Common (decimal)
-
+import Data.List (nub, (\\), foldl')
+import Data.Set qualified as S
+import Debug.Trace
 -- /** Utilities
 type Parser a = RE Char a
 
@@ -18,7 +20,40 @@ inc v = plus v 1
 dec v = plus v (-1)
 -- **/
 
-type Input = String
+type Coord = (Int, Int)
+type Walls = S.Set Coord
+
+dfs, bfs :: Eq a => a -> (a -> Bool) -> (a -> [a]) -> [a]
+bfs x isGoal adj
+  | isGoal x  = [x]
+  | otherwise = genericSearch [[x]] [] isGoal adj (\open newOpen -> open ++ newOpen)
+dfs x isGoal adj
+  | isGoal x  = [x]
+  | otherwise = genericSearch [[x]] [] isGoal adj (\open newOpen -> newOpen ++ open)
+
+genericSearch :: Eq a => [[a]] -> [[a]] -> (a -> Bool) -> (a -> [a]) -> ([[a]] -> [[a]] -> [[a]]) -> [a]
+genericSearch open closed isGoal adj modifyOpen
+  | length solution > 0 =  head solution
+  | otherwise = genericSearch (modifyOpen pathList newOpen) newClosed isGoal adj modifyOpen
+  where
+    currPath@(currState:_) = head open
+    newClosed = currPath:closed
+    pathList = tail open
+    newOpen = map (\x -> x:currPath) (adj currState) \\ newClosed
+    solution = filter (\(st:_) -> isGoal st) $ newOpen
+
+walls :: [String] -> Walls
+walls str = S.fromList $ [(x,y) | (y, raw) <- zip [-1..] str,
+                     (x, ch) <- zip [-1..] raw, ch == '#']
+
+deltas :: [Coord]
+deltas = [(-1,0), (0,1), (0,-1), (1,0)]
+
+adjacents :: Walls -> Coord -> [Coord]
+adjacents walls coord = filter (not . flip S.member walls) (map (getAdjacents coord) deltas)
+  where getAdjacents (x1,y1) (x2,y2) = (x1+x2, y1+y2)
+
+type Input = Walls
 
 getInput :: FilePath -> IO Input
 getInput filepath = do
@@ -26,10 +61,10 @@ getInput filepath = do
   return input
 
 prepare :: String -> Input
-prepare = undefined
+prepare = walls . lines
 
-part1 :: Input -> Int
-part1 input = 42
+part1 :: Walls -> [Coord]
+part1 w = bfs (0,0) (\x -> x == (6,2)) (adjacents w)
 
 part2 :: Input -> Int
 part2 input = 42
@@ -39,3 +74,4 @@ main = do
   input <- getInput "./input.txt"
   putStrLn $ "Part 1: " <> show (part1 input)
   putStrLn $ "Part 2: " <> show (part2 input)
+
