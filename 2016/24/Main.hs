c84f8dbc3e1a46c716fc9ca2224fbdfa38346567
Dmitry Kvasnikov <dmitry.kvasnikov@gmail.com>
2024-05-03 17:21:03 +0200

    AOC 2016 Day 24 Solution

--- 2016/24/Main.hs
+++ 2016/24/Main.hs
@@ -1,59 +1,106 @@
 module Main where
 
-import Data.Maybe (fromMaybe, catMaybes)
-import Text.Regex.Applicative (RE, match, (=~), sym, psym, string, many, some, (<|>))
-import Text.Regex.Applicative.Common (decimal)
-import Data.List (nub, (\\), foldl')
+import Data.Maybe (fromMaybe, catMaybes, fromJust)
+import Data.List (permutations, (\\), foldl')
 import Data.Set qualified as S
-import Debug.Trace
--- /** Utilities
-type Parser a = RE Char a
-
-sepBy :: Parser a -> Parser b -> Parser [a]
-sepBy p s = (:) <$> p <*> many (s *> p)
-
-plus, minus :: forall a . (Enum a, Bounded a) => a -> Int -> a
-plus v n = toEnum ((fromEnum v + n) `mod`  (fromEnum (maxBound :: a) + 1))
-minus v n = plus v (-n)
-inc, dec :: forall a . (Enum a, Bounded a) => a -> a
-inc v = plus v 1
-dec v = plus v (-1)
--- **/
+import Data.Map.Strict qualified as M
+import Data.Foldable (Foldable(toList))
 
 type Coord = (Int, Int)
-type Walls = S.Set Coord
+type Points = S.Set Coord
+type Walls = Points
+type Queue = Points
+type Seen = Points
+type Pattern = String
+type Input = [String]
+type DP = M.Map Coord Int
+type DM = M.Map (Coord, Coord) Int
+type Route = [Coord]
 
-dfs, bfs :: Eq a => a -> (a -> Bool) -> (a -> [a]) -> [a]
-bfs x isGoal adj
-  | isGoal x  = [x]
-  | otherwise = genericSearch [[x]] [] isGoal adj (\open newOpen -> open ++ newOpen)
-dfs x isGoal adj
-  | isGoal x  = [x]
-  | otherwise = genericSearch [[x]] [] isGoal adj (\open newOpen -> newOpen ++ open)
+(!+!) :: Coord -> Coord -> Int
+(!+!) (x1, y1) (x2, y2) = abs (x2 - x1) + abs (y2 - y1)
 
-genericSearch :: Eq a => [[a]] -> [[a]] -> (a -> Bool) -> (a -> [a]) -> ([[a]] -> [[a]] -> [[a]]) -> [a]
-genericSearch open closed isGoal adj modifyOpen
-  | length solution > 0 =  head solution
-  | otherwise = genericSearch (modifyOpen pathList newOpen) newClosed isGoal adj modifyOpen
+dp :: Walls -> Coord -> Coord -> Int
+dp w b e = go (getAdjacents w b) (S.singleton b) (M.singleton b 0)
   where
-    currPath@(currState:_) = head open
-    newClosed = currPath:closed
-    pathList = tail open
-    newOpen = map (\x -> x:currPath) (adj currState) \\ newClosed
-    solution = filter (\(st:_) -> isGoal st) $ newOpen
+    go :: Queue -> Seen -> DP -> Int
+    go q seen dp
+      | S.null q = fromJust . M.lookup e $ dp
+      -- | cand == e = minDist + 1
+      | otherwise = go (S.union adj' $ S.delete cand q) (S.union seen adj') (M.insert cand (minDist + 1) dp)
+      where
+        cand = bestCand e q
+        adj = getAdjacents w cand
+        adj' = S.filter (not . flip S.member seen) adj
+        minDist = getMinDist dp adj
 
-walls :: [String] -> Walls
-walls str = S.fromList $ [(x,y) | (y, raw) <- zip [-1..] str,
-                     (x, ch) <- zip [-1..] raw, ch == '#']
+dp1 :: Walls -> Points -> DM
+dp1 w p = foldl' go M.empty (S.toList p)
+  where go :: DM -> Coord -> DM
+        go dm point = go' dm [[point]] (S.empty)
+          where go' :: DM -> [[Coord]] -> Points -> DM
+                go' dm [] _ = dm
+                go' dm (q:qs) seen = go' newDm newQueue newSeen
+                  where curr :: Coord
+                        curr = head q
+                        adj = S.filter (not . flip S.member seen) $ getAdjacents w curr
+                        newDm = if (S.member curr p) && (length q > 1)
+                                   then M.insert (curr, point) (length q - 1) dm
+                                   else dm
+                        newSeen = S.union seen adj
+                        newQueue = qs ++ (map (\x -> x : q) (S.toList adj))
+
+getDistMatrix :: Walls -> Points -> DM
+getDistMatrix w p =
+  let ps = pairs p
+  in M.fromList (map go ps)
+    where go (c1,c2) = ((c1,c2), dp w c1 c2)
+
+pairs :: Points -> [(Coord, Coord)]
+pairs = go . toList
+  where go :: [Coord] -> [(Coord, Coord)]
+        go [] = []
+        go [x] = []
+        go (x:xs) = map ((,) x) xs ++ go xs
+
+getAdjacents :: Walls -> Coord -> Points
+getAdjacents w c = S.fromList . filter (not . flip S.member w) .  map (go c) $ deltas
+  where go (x1, y1) (x2,y2) = (x1 + x2, y1 + y2)
+
+getCoords :: Pattern -> [String] -> S.Set Coord
+getCoords pat grid = S.fromList $ [(x,y) | (y, raw) <- zip [-1..] grid,
+                     (x, ch) <- zip [-1..] raw, elem ch pat]
+
+getMinDist :: DP -> Points -> Int
+getMinDist dp = minimum . catMaybes . map (flip M.lookup dp) . S.toList
+
+bestCand :: Coord -> Points -> Coord
+bestCand coord ps = snd (S.foldl' go (maxBound :: Int, (0,0)) ps)
+  where go acc@(d, c) cand = let d' = cand !+! coord
+                             in if d' < d then (d', cand)
+                                          else acc
+
+getRouteLength :: DM -> Route -> Int
+getRouteLength _ [] = 0
+getRouteLength _ [x] = 0
+getRouteLength d (x:y:z) = fromJust (M.lookup (x,y) d) + (getRouteLength d (y:z))
+
+getLength :: DM -> Coord -> Coord -> Int
+getLength dm c1 c2 = case (M.lookup (c1,c2) dm) of
+                       Just d -> d
+                       Nothing -> fromJust (M.lookup (c2,c1) dm)
+
+walls :: Input -> Walls
+walls = getCoords "#"
+
+points :: Input -> Points
+points = getCoords "123456789"
+
+zero :: Input -> Coord
+zero = head . S.toList . getCoords "0"
 
 deltas :: [Coord]
 deltas = [(-1,0), (0,1), (0,-1), (1,0)]
-
-adjacents :: Walls -> Coord -> [Coord]
-adjacents walls coord = filter (not . flip S.member walls) (map (getAdjacents coord) deltas)
-  where getAdjacents (x1,y1) (x2,y2) = (x1+x2, y1+y2)
-
-type Input = Walls
 
 getInput :: FilePath -> IO Input
 getInput filepath = do
@@ -61,13 +108,25 @@ getInput filepath = do
   return input
 
 prepare :: String -> Input
-prepare = walls . lines
+prepare = lines
 
-part1 :: Walls -> [Coord]
-part1 w = bfs (0,0) (\x -> x == (6,2)) (adjacents w)
+part1 :: Input -> Int
+part1 input = let w = walls input
+                  p = points input
+                  z = zero input
+                  -- dm = getDistMatrix w (S.insert z p)
+                  dm = dp1 w (S.insert z p)
+                  routes = map (z:) (permutations $ S.toList p)
+               in minimum . map (getRouteLength dm) $ routes
 
 part2 :: Input -> Int
-part2 input = 42
+part2 input = let w = walls input
+                  p = points input
+                  z = zero input
+                  -- dm = getDistMatrix w (S.insert z p)
+                  dm = dp1 w (S.insert z p)
+                  routes = map (\r -> [z] ++ r ++ [z]) (permutations $ S.toList p)
+               in minimum . map (getRouteLength dm) $ routes
 
 main :: IO()
 main = do
