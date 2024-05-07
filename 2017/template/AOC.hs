682bf6729a4fed37383a8612fca36b5c6653fdeb
Dmitry Kvasnikov <dmitry.kvasnikov@gmail.com>
2024-05-07 09:31:34 +0200

    template

--- 2017/template/AOC.hs
+++ 2017/template/AOC.hs
@@ -1,3 +1,9 @@
 module AOC where
 
 type Coord = (Int, Int)
+
+(<+>) :: Coord -> Coord -> Coord
+(<+>) (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
+
+manhattan :: Coord -> Coord -> Int
+manhattan (x1,y1) (x2,y2) = abs (x2 - x1) + abs (y2 - y1)
